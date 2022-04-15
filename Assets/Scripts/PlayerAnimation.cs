81d4577d10664bf07ce5df09f1b742b22faca349
Julaxe <julaxe8@gmail.com>
2022-04-15 11:08:21 -0400

    muzzle flash effect added

--- Assets/Scripts/PlayerAnimation.cs
+++ Assets/Scripts/PlayerAnimation.cs
@@ -6,9 +6,11 @@ public class PlayerAnimation : MonoBehaviour
 {
     private Rigidbody _rigidbody;
     private Animator _animator;
+    private PlayerWeapon _weapon;
 
     private readonly int hashVelXAnimation = Animator.StringToHash("velX");
     private readonly int hashVelYAnimation = Animator.StringToHash("velY");
+    private readonly int hashShootingAnimation = Animator.StringToHash("Shooting");
 
     [SerializeField] private Vector3 cross;
     [SerializeField] private float dotprod;
@@ -17,10 +19,17 @@ public class PlayerAnimation : MonoBehaviour
     {
         _rigidbody = GetComponent<Rigidbody>();
         _animator = GetComponent<Animator>();
+        _weapon = GetComponent<PlayerWeapon>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        UpdateFacingDirection();
+        _animator.SetBool(hashShootingAnimation, _weapon.isShooting);
+    }
+
+    private void UpdateFacingDirection()
     {
         var normalizedVel = _rigidbody.velocity.normalized;
         var forward = transform.forward;
@@ -30,4 +39,5 @@ public class PlayerAnimation : MonoBehaviour
         _animator.SetFloat(hashVelXAnimation, cross.y);
         _animator.SetFloat(hashVelYAnimation, dotprod);
     }
+    
 }
--- Assets/Scripts/PlayerWeapon.cs
+++ Assets/Scripts/PlayerWeapon.cs
@@ -2,13 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.VFX;
 
 public class PlayerWeapon : MonoBehaviour
 {
+    [Header("Muzzle")]
+    [SerializeField] private VisualEffect _muzzleFlash;
     [SerializeField] private GameObject _muzzleLocation;
-    [SerializeField] private GameObject _weaponRef;
 
+    
     public bool isShooting;
+
+    [Header("Weapon Specs")] 
+    [SerializeField] private GameObject _weaponRef;
+    [SerializeField]private float _range;
+    [SerializeField]private int _clipSize;
+    
     public UnityAction Reload;
     void Start()
     {
@@ -24,7 +33,19 @@ public class PlayerWeapon : MonoBehaviour
 
     public void Shoot()
     {
-        
+        Debug.Log("shoot");
+
+        _muzzleFlash.Play();
+        // //spawn muzzle
+        // Vector3 shootDirection = transform.forward;
+        // // GameObject muzzle = Instantiate(_muzzleRef, _muzzleLocation.transform.position,
+        // //     Quaternion.FromToRotation(Vector3.up, shootDirection));
+        // // Destroy(muzzle, 1.0f);
+        // RaycastHit hit;
+        // if (Physics.Raycast(_muzzleLocation.transform.position, shootDirection, out hit, _range))
+        // {
+        //     
+        // }
     }
     
 }
